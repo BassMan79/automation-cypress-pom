47a8a474823f46b4f21bb98314f6b9608c627d8b
Facundo Fernandez <facu.fernandez1987@gmail.com>
2024-03-14 13:56:25 +0100

    mochawesome-reporter

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   retries: 1,
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
